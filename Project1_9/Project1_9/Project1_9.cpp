420da8572fd087eced137bcc112030a0961d0395
JeongSooHyeon <82567002+JeongSooHyeon@users.noreply.github.com>
2021-06-23 13:41:38 +0900

    1-5 ~ 1-10

---
+++ Project1_9/Project1_9/Project1_9.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A, B;
+
+	cin >> A >> B;
+
+	cout << A + B << endl;
+	cout << A - B << endl;
+	cout << A * B << endl;
+	cout << A / B << endl;
+	cout << A % B << endl;
+
+	return 0;
+}
