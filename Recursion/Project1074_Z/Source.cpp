4d5bb678177741ba7cee25cbb662bff8a85fade9
SooHyeon Jeong <jsh75721@naver.com>
2023-07-14 01:59:12 +0900

    문제1074-Z 완전탐색으로 푸는데 마무리가 안 됨. 재귀 어떻게 벗어날까

--- Recursion/Project1074_Z/Source.cpp
+++ Recursion/Project1074_Z/Source.cpp
@@ -8,37 +8,38 @@ using namespace std;
 다음 조각 : 
 */
 int n, r, c;	// 1<=n<=15, 0<=r,c<=2^n
-
+bool found = false;
 /*
 i,j i,j+1 i+1,j i+1,j+1
 */
-int tempCoordi[4][2] = { {0,0}, {0,1}, {1,0},{1,1} };
+int tempCoordi[4][2] = { {0,0}, {0,1}, {1,0}, {1,1} };
 
 int solve(int row, int col, int num) {
-	int ret = -1;
-	int tempRow, tempCol;
+	long long int ret = 0;
+	long long int tempRow, tempCol;
 	if (num == 0) {
 		return 0;
 	}
 
 
 	for (int i = 0; i < 4; i++) {
-		tempRow = row + tempCoordi[i][0] * num;
-		tempCol = col + tempCoordi[i][1] * num;
+		tempRow = row + tempCoordi[i][0] * (int)pow(2, num-1);
+		tempCol = col + tempCoordi[i][1] * (int)pow(2, num-1);
 		if (num == 1) {
+
 			ret++;
 			if (r == tempRow && c == tempCol) {	// 찾음
 				cout << "찾음" << ret << endl;
 				return ret;
 			}
 		}
-		ret += solve(tempRow, tempCol, num / 2);
+		ret += solve(tempRow, tempCol, num-1);
 	}
 	return ret;
 }
 
 int main() {
 	cin >> n >> r >> c;
-	cout << solve(0, 0, n);
+	cout << solve(0, 0, n)-1;
 
 }
