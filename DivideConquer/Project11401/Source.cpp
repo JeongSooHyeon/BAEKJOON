53ba152116004e258046d8dfe7d315586865ee3a
SooHyeon Jeong <jsh75721@naver.com>
2023-07-23 01:25:18 +0900

    이항계수3 진행 중 모듈러 연산, 페르마의 소정리 다시

--- DivideConquer/Project11401/Source.cpp
+++ DivideConquer/Project11401/Source.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int modulo(int n, int k) {
-
+#define C 1000000007
+long long Power(int x) {
+	if (x == 0)
+		return 1;
+	if (x % 2 == 1)
+		return B * Power(x - 1) % C;
+	else {
+		int half = Power(x / 2);
+		return half * half % C;
+	}
 }
 
 int main() {
-
-	int n, k;
+long long n, k, A, B;
 	cin >> n >> k;
+	A = 1;
+	// A : (n-k+1) * ... * n
+	for (int i = n - k + 1; i <= n; ++i) {
+		A = (A * i) % C;
+	}
 
+	// B : 1 * ... * k
+	for (int i = 1; i <= k; i++) {
+		B = (B * i) % C;
+	}
 
+	// B = (k!)^(p-2)
+	B = Power(C - 2);
+
+	cout << A * B % C;
+
+	return 0;
 
 }
