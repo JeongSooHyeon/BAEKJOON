67b96479a1a726934b92757e99ea5de877d4e042
SooHyeon Jeong <jsh75721@naver.com>
2023-07-24 04:34:07 +0900

    이항 계수3 끝

--- DivideConquer/Project11401/Source.cpp
+++ DivideConquer/Project11401/Source.cpp
@@ -2,35 +2,51 @@
 using namespace std;
 
 #define C 1000000007
-long long Power(int x) {
-	if (x == 0)
-		return 1;
-	if (x % 2 == 1)
-		return B * Power(x - 1) % C;
-	else {
-		int half = Power(x / 2);
-		return half * half % C;
+
+long long int factorial(int x) {
+	long long int ans = 1;
+	for (int i = 2; i <= x; i++) {
+		ans = ans * i % C;
 	}
+	return ans;
+}
+
+long long int power(int x, int y) {
+	if (y == 0)
+		return 1;
+
+	if (y % 2)
+		return (power(x, y - 1) * x) % C;
+
+	long long int temp = power(x, y / 2) % C;
+	return (temp * temp) % C;
+
 }
 
 int main() {
-long long n, k, A, B;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, k;
+	long long int  A = 1, B = 1;
 	cin >> n >> k;
-	A = 1;
-	// A : (n-k+1) * ... * n
-	for (int i = n - k + 1; i <= n; ++i) {
-		A = (A * i) % C;
-	}
 
-	// B : 1 * ... * k
-	for (int i = 1; i <= k; i++) {
-		B = (B * i) % C;
-	}
+	// A : n! % C
+	//for (int i = 2; i <= n; i++) 
+	//	A = A * i % C;
 
-	// B = (k!)^(p-2)
-	B = Power(C - 2);
+	//// B : (k!(n-k)!)^C-2 % C
+	for (int i = 2; i <= k; i++)
+		B = B * i % C;
+	for (int i = 2; i <= n-k; i++)
+		B = B * i % C;
+	A = factorial(n);
 
-	cout << A * B % C;
+	// B : (k!(n-k)!)^C-2 % C
+
+	B = A * power(B, C - 2) % C;
+
+	cout << B;
 
 	return 0;
 
