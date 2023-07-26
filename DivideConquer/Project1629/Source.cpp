980083cf7a3007cf1234f57d8bd94c784339bd0c
SooHyeon Jeong <jsh75721@naver.com>
2023-07-26 21:32:26 +0900

    분활정복 - 곱셈 끝

--- DivideConquer/Project1629/Source.cpp
+++ DivideConquer/Project1629/Source.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int solve(long long a, long long b, long long c) {
+long long c;
 
+long long power(long long a, long long b) {
+	// ±âÀú »ç·Ê
+	if (b == 1)
+		return a % c;
+
+	long long temp = power(a, b / 2);
+
+	if (b % 2 == 1) {	// È¦¼ö
+		return ((temp % c) * (a*temp % c))%c;
+	}
+
+	return ((temp % c) * (temp % c))%c;
 }
 
 int main() {
-
-	long long a, b, c;
+	long long a, b;
 	cin >> a >> b >> c;
 
-
+	cout << power(a, b);
 }
