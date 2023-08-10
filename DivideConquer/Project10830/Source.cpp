13fd0c8c36f95750853f369ad437935e15219e2b
SooHyeon Jeong <jsh75721@naver.com>
2023-08-10 21:55:55 +0900

    분할 정복 행렬 제곱

---
+++ DivideConquer/Project10830/Source.cpp
@@ -0,0 +1,23 @@
+/*
+Çà·Ä Á¦°ö
+*/
+#include <iostream>
+using namespace std;
+
+long long n, b;
+
+
+void solved() {
+
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	cin >> n >> b;
+
+
+
+	return 0;
+}
