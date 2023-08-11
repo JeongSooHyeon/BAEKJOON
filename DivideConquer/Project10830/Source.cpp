746d84ad6f208f2e1e02dd6f808b67d6c781d036
SooHyeon Jeong <jsh75721@naver.com>
2023-08-11 23:36:08 +0900

    분할정복 행렬 제곱 다시 풀기

--- DivideConquer/Project10830/Source.cpp
+++ DivideConquer/Project10830/Source.cpp
@@ -5,10 +5,26 @@
 using namespace std;
 
 long long n, b;
+int a[5][5];
+int result[5][5];
 
+void solve(int v1[5][5], int v2[5][5]) {
+	int temp[5][5] = { {0,}, };
 
-void solved() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				temp[i][j] += v1[i][k] * v2[k][j];
+			}
+			temp[i][j] %= 1000;
+		}
+	}	
 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			v1[i][j] = temp[i][j];
+		}
+	}
 }
 
 int main() {
@@ -17,7 +33,28 @@ int main() {
 
 	cin >> n >> b;
 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			result[i][j] = 0;
+			cin >> a[i][j];
+		}
+		result[i][i] = 1;
+	}
 
+	while (b) {	// b가 1일 때까지
+		if (b % 2 == 1) {
+			solve(result, a); 
+		}
+		solve(a, a); 
+		b /= 2;
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << result[i][j] << ' ';
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
