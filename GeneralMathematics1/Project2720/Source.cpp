1c0232e13a54b69f3a7cee36729e4bfa0a05dd98
SooHyeon Jeong <jsh75721@naver.com>
2023-08-30 23:58:50 +0900

    일반 수학1 - 세탁소 사장 동혁

---
+++ GeneralMathematics1/Project2720/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 거스름돈 <= $5.00
+// 동전의 개수 최소화
+// Quarter : 0.25, Dime : 0.10, Nickel : 0.05, Penny : 0.01
+
+#define double Quarter 0.25
+
+void Calc(int c) {
+
+
+}
+
+int main() {
+	int t;	// 테스트 케이스
+	int c;	// 거스름돈, 센트, 1 <= c <= 500
+	vector<int> result(4);
+
+
+
+
+
+	return 0;
+}
