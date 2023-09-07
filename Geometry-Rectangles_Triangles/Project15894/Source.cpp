82965ecc0fe0e1b65157e8a240c902adad8e36c1
SooHyeon Jeong <jsh75721@naver.com>
2023-09-08 02:10:25 +0900

    기하:직사각형과 삼각형 - 수학은 체육과목입니다

---
+++ Geometry-Rectangles_Triangles/Project15894/Source.cpp
@@ -0,0 +1,16 @@
+/*
+수학은 체육과목입니다
+*/
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	long long n;
+	cin >> n;
+
+	cout << n * 4;
+
+	return 0;
+}
---
+++ Geometry-Rectangles_Triangles/Project9063/Source.cpp
@@ -0,0 +1,23 @@
+/*
+´ëÁö
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main() {
+	int n;
+	cin >> n;
+	
+	int w, h;
+	int x, y;
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		if()
+	}
+
+
+	return 0;
+}
